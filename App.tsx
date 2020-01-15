745f0184be628ccc5c6f65d26a87495bc6a52451
marlenekoh <marlenekohxml@gmail.com>
2020-01-15 20:19:06 +0800

    Use styled-components for View

--- App.tsx
+++ App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+
+import { AppView } from "./AppView"
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <AppView>
       <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    </AppView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
