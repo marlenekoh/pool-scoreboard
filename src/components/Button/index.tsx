7cd4e1d19da1b98c5e2d1d36b17f0bb5ea63a3ba
marlenekoh <marlenekohxml@gmail.com>
2020-01-18 13:35:18 +0800

    Change Buttons library

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,9 @@
-import styled from "styled-components/native";
+import React from "react";
+import { Button as ReactNativeButton } from "react-native-elements";
+
+interface ButtonProps extends React.ComponentProps<typeof ReactNativeButton> {}
 
 // TODO: style buttons
-export const Button = styled.Button``;
+export const Button: React.FunctionComponent<ButtonProps> = props => {
+  return <ReactNativeButton {...props} />;
+};
