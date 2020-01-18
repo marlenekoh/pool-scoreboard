8dc22c607179c544f4648785fea6a0f31c9d2c88
marlenekoh <marlenekohxml@gmail.com>
2020-01-18 15:05:03 +0800

    Add settings menu placeholder

---
+++ src/screens/GameScreen/Settings/SettingsMenu/SettingsMenuContainer.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+export const SettingsMenuContainer = styled.View`
+  position: absolute;
+  bottom: 75;
+  right: 25;
+
+  width: 150px;
+  height: 130px;
+  border-radius: 15px;
+  background: white;
+`;
